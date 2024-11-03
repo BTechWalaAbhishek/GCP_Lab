63bfdd342f0ea98b2e5f782fde8b0d679f1a795c
BTechWalaAbhishek <abhi001101@gmail.com>
2024-11-03 20:58:20 +0530

    Create GSP207.sh

---
+++ Dataflow: Qwik Start - Python/GSP207.sh
@@ -0,0 +1,21 @@
+gcloud config set compute/region $LOCATION
+gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket/
+gcloud services disable dataflow.googleapis.com
+sleep 20
+gcloud services enable dataflow.googleapis.com
+sleep 20
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e LOCATION=$LOCATION python:3.9 /bin/bash -c '
+pip install "apache-beam[gcp]"==2.42.0 && \
+python -m apache_beam.examples.wordcount --output OUTPUT_FILE && \
+HUSTLER=gs://$DEVSHELL_PROJECT_ID-bucket && \
+python -m apache_beam.examples.wordcount --project $DEVSHELL_PROJECT_ID \
+  --runner DataflowRunner \
+  --staging_location $HUSTLER/staging \
+  --temp_location $HUSTLER/temp \
+  --output $HUSTLER/results/output \
+  --region $LOCATION
+'
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
